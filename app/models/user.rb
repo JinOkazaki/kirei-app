ec5c2f5bd288889a84430ca1c75d07b2128814f1
JinOkazaki <o.jin19951212@icloud.com>
2021-10-02 13:41:14 +0900

    ビューファイル編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,15 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :gender
+
+  with_options presence: true do
+    validates :nickname
+    validates :age, format: {with: /\A[0-9]{2,3}\z/}
+    validates :gender_id, numericality: {other_than: 1, message: "can't be blank"}
+  end
+  validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "is invalid. Include both letters and numbers."}
+  
 end
