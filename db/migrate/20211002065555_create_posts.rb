cd3491c54253f7a6beeef2336eacf021fe8274e9
JinOkazaki <o.jin19951212@icloud.com>
2021-10-04 19:41:47 +0900

    commentsテーブルの作成

--- db/migrate/20211002065555_create_posts.rb
+++ db/migrate/20211002065555_create_posts.rb
@@ -3,7 +3,7 @@ class CreatePosts < ActiveRecord::Migration[6.0]
     create_table :posts do |t|
       t.text       :caption, null: false
       t.integer    :category_id, null: false
-      t.references :user, null: false, foreign: true
+      t.references :user, null: false, foreign_key: true
       t.timestamps
     end
   end
