d71a9ac0b1590a2f7837bcc5523d580152ae077a
JinOkazaki <o.jin19951212@icloud.com>
2021-10-06 18:14:09 +0900

    likesテーブル作成

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,11 +4,16 @@ class Post < ApplicationRecord
 
   belongs_to :user
   has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
   has_one_attached :image
 
   with_options presence: true do
     validates :caption
     validates :image
     validates :category_id, numericality: {other_than: 1, message: "can't be blank"}
+  end
+
+  def liked_by?(user)
+    likes.where(user.id: user.id).exists?
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ApplicationRecord
 
   has_many :posts
   has_many :comments
+  has_many :likes
   
   with_options presence: true do
     validates :nickname
