577ab4ad37fe5c62b5107eb83f60635435e4b9b2
JinOkazaki <o.jin19951212@icloud.com>
2021-10-08 19:23:03 +0900

    ルーティング、コントローラーの設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,18 @@ class UsersController < ApplicationController
     @posts = current_user.posts.order("created_at DESC").page(params[:page]).per(2)
   end
 
+  def index
+    users = User.where.not(id: current_user.id)
+  end
+
+  def followings
+    user = User.find(paraml[:id])
+    @users = user.followings
+  end
+
+  def followers
+    user = User.find(params[:id])
+    @users = user.followers
+  end
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ApplicationRecord
   has_many :likes
   has_many :relationships, foreign_key: :following_id
   has_many :followings, through: :relationships, source: :follower
-  has_many :reverse_of_relationships: 'Relationship', foreign_key: :follower_id
+  has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: :follower_id
   has_many :followers, through: :reverse_of_relationships, source: :following
 
 
