b4ff217df3ee2774a2c08e75a70fe77cdde3d156
JinOkazaki <o.jin19951212@icloud.com>
2021-10-14 15:21:37 +0900

    いいね一覧ページ編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,7 +25,23 @@ class UsersController < ApplicationController
   def likes
     @user = User.find(params[:id])
     likes = Like.where(user_id: @user.id).pluck(:post_id)
-    @like_posts = Post.find(likes)
+    @like_posts = Post.where(id: likes).order("created_at DESC").page(params[:page]).per(2)
+    @currentUserEntry = Entry.where(user_id: current_user.id)
+    @userEntry = Entry.where(user_id: @user.id)
+    unless @user.id == current_user.id
+      @currentUserEntry.each do |cu|
+        @userEntry.each do |u|
+          if cu.room_id == u.room_id then
+            @isRoom = true
+            @roomId = cu.room_id
+          end
+        end
+      end
+      unless @isRoom
+        @room = Room.new
+        @entry = Entry.new
+      end
+    end
   end
 
   def followings
