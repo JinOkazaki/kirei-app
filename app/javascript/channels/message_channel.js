9b942733f4687556ca313b6af26392695ffa74a1
JinOkazaki <o.jin19951212@icloud.com>
2021-10-15 21:41:26 +0900

    チャット画面の表示修正

--- app/javascript/channels/message_channel.js
+++ app/javascript/channels/message_channel.js
@@ -16,28 +16,27 @@ consumer.subscriptions.create("MessageChannel", {
       const D = ("00" + date.getDate()).slice(-2)
       const h = ("00" + date.getHours()).slice(-2)
       const m = ("00" + date.getMinutes()).slice(-2)
-      const s = ("00" + date.getSeconds()).slice(-2)
       if (data.message.message == "") {
         alert("メッセージを入力してください!");
       } else {
-      const html = `
+        const currentUserId = document.getElementById("chat-room").dataset.userId;
+        const htmlClass = currentUserId == data.message.user_id ? "" : "mycomment"
+        const htmlClassJs = currentUserId == data.message.user_id ? "" : "mycomments"
+        const orderOne = currentUserId == data.message.user_id ? "" : "order-one"
+        // const orderTwo = currentUserId == data.message.user_id ? "" : "order-two"
+        const html = `
             <div class="chat-hukidashi">
-              <% if m.user.id == current_user.id %>
-                <div class="current-message-time">
-                  <span class="time">${Y}/${M}/${D} ${h}:${m}</span>
-                  <span class="current-message">${data.message.message}</span>
-                </div>
-              <% else %>
-                <div class="message-time">
-                  <span class="message">${data.message.message}</span>
-                  <span class="time">${Y}/${M}/${D} ${h}:${m}</span>
-                </div>
-              <% end %>
+              <div class="current-message-time ${htmlClassJs}">
+                <span class="time ${orderOne}">${Y}/${M}/${D} ${h}:${m}</span>
+                <span class="current-message ${htmlClass}">${data.message.message}</span>
+              </div>
             </div>`;
-      const messages = document.getElementById('message-list');
-      const newMessage = document.getElementById('message-form');
-      messages.insertAdjacentHTML('beforeend',html);
-      newMessage.value = '';
+        const messages = document.getElementById('message-list');
+        const newMessage = document.getElementById('message-form');
+        messages.insertAdjacentHTML('beforeend',html);
+        newMessage.value = '';
       };
   }
 });
+
+
