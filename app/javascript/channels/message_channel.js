acec1bf574dc1c67c286de4995837a4371370c08
JinOkazaki <o.jin19951212@icloud.com>
2021-10-15 12:05:32 +0900

    修正1

--- app/javascript/channels/message_channel.js
+++ app/javascript/channels/message_channel.js
@@ -17,16 +17,27 @@ consumer.subscriptions.create("MessageChannel", {
       const h = ("00" + date.getHours()).slice(-2)
       const m = ("00" + date.getMinutes()).slice(-2)
       const s = ("00" + date.getSeconds()).slice(-2)
+      if (data.message.message == "") {
+        alert("メッセージを入力してください!");
+      } else {
       const html = `
             <div class="chat-hukidashi">
-              <div class="current-message-time">
-                <span class="time">${Y}/${M}/${D} ${h}:${m}</span>
-                <span class="current-message">${data.message.message}</span>
-              </div>
+              <% if m.user.id == current_user.id %>
+                <div class="current-message-time">
+                  <span class="time">${Y}/${M}/${D} ${h}:${m}</span>
+                  <span class="current-message">${data.message.message}</span>
+                </div>
+              <% else %>
+                <div class="message-time">
+                  <span class="message">${data.message.message}</span>
+                  <span class="time">${Y}/${M}/${D} ${h}:${m}</span>
+                </div>
+              <% end %>
             </div>`;
       const messages = document.getElementById('message-list');
       const newMessage = document.getElementById('message-form');
       messages.insertAdjacentHTML('beforeend',html);
       newMessage.value = '';
+      };
   }
 });
