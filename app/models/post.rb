d93d7a58cdb68f978e74b93da20669862a213724
JinOkazaki <o.jin19951212@icloud.com>
2021-10-18 15:16:07 +0900

    各テーブルアソシエーション

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,6 +6,7 @@ class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_one_attached :image
+  has_many :notifications, dependent: :destroy
 
   with_options presence: true do
     validates :caption
